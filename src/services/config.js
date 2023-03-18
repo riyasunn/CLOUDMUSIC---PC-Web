62c9df885a117e90f8ccea1c56fdbf4690a132cd
Riya Sun <riya.sunn@gmail.com>
2023-03-18 15:37:48 +1300

    added proBaseURL

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 const devBaseURL = "http://123.207.32.32:9002/";
-const proBaseURL = "";
+const proBaseURL = "http://123.207.32.32:9002/";
 
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL;
 
