1bf16cd7f879721cf82c78154c9782f28ed6d034
Riya Sun <riya.sunn@gmail.com>
2023-03-18 16:12:34 +1300

    updated devBaseURL

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
-const devBaseURL = "http://123.207.32.32:9002/";
-const proBaseURL = "http://123.207.32.32:9002/";
+const devBaseURL = "https://netease-cloud-music-api-self-nu.vercel.app/";
+const proBaseURL = "https://netease-cloud-music-api-self-nu.vercel.app/";
 
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL;
 
